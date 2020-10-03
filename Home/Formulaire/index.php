46a361cb1f9cb544ee1c26151fd278af32f02bbd
Abdellah2019 <cheikhelmoustaphea@gmail.com>
2020-10-03 19:34:44 +0200

    New Essai

---
+++ Home/Formulaire/index.php
@@ -0,0 +1,19 @@
+<?php
+    echo "hello in php";
+    /*
+        mysql_connect("localhost","root","") or die("Erreur");
+        mysql_select_db("mydatabase") or die("La base de donnée est introuvable.");
+        if($_POST){
+            $pseudo=$_POST['pseudo'];
+            $password=$_POST['password'];
+        }
+        if(!empty($pseudo) AND !empty($password)){
+            mysql_query('INSERT INTO users (pseudo,password) VALUES ("'.$pseudo.'","'.$passord.'")') or die("Erreurr".mysql_error());
+            echo "OK!";
+        }
+        else{
+            echo "Erreur ,un ou plusieurs champs invalide!"
+        }
+        """
+    */
+?>
